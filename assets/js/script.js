6425cad6c8d62e6bca4ccf82d5949e896ae4b7de
Anon_Dev <bhaskarjadhav9666@gmail.com>
2026-02-06 23:59:37 +0530

    Updated UI and improved CSS design

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,63 @@
- 
-
+//     chrome://extensions/
  let myLeads=[]
  const inputEl= document.getElementById("input-el") // you cant reassignment the input varible ...
  const inputBtn=document.getElementById("input-btn")
  const ulEl=document.getElementById("ulEl")
+ const deleteBtn=document.getElementById("delete-btn")
+
+ const leadsFromLocalStorage= JSON.parse(localStorage.getItem("myLeads"))
+ console.log( leadsFromLocalStorage)
+
+if( leadsFromLocalStorage){
+   myLeads= leadsFromLocalStorage
+   renderlead() 
+
+}
+function renderlead()
+{
+  let listItem=""
+  for (let i=0; i < myLeads.length;i++)
+   {
+       // log out the item in the my leasds array using for loop
+       // listItem += "<li><a href='" + myLeads[i] + "' target='_blank'>" + myLeads[i] + "</a></li>";
+      listItem += `<li><a href="${myLeads[i]}"target='blank'>
+      ${myLeads[i]}</a></li>`;
+   
+   }
+      ulEl.innerHTML= listItem
+
+}
+
+   deleteBtn.addEventListener("dblclick",function(){
+   console.log("double clicked ")
+   localStorage.clear()
+      myLeads=[]
+      renderlead()
 
 
- inputBtn.addEventListener("click",function(){
- myLeads.push(inputEl.value)
- //clear out the input filed 
-//  ulEl.innerHTML+="<li>"+inputEl.value+ "</li>" /// SIMPLEST WAY 
- renderlead()
- inputEl.value=""
+})
+
+
+
+
+inputBtn.addEventListener("click",function(){
+  myLeads.push(inputEl.value)
+// save the mylead arrray to  the local storage 
+  localStorage.setItem("myLeads",JSON.stringify(myLeads))
+  renderlead()
+  inputEl.value=""  //clear out the input filed 
+ // to verify that it works
+  console.log ( localStorage.getItem("myleads"))
  })
+
+//render lead type1
+
+
+
+
+
+
+
 
 
 
@@ -23,19 +68,26 @@
 //        let listItem="<li>"+inputEl.value+ "</li>"
 //     ulEl.innerHTML+=listItem
 //     }
+//  ulEl.innerHTML+="<li>"+inputEl.value+ "</li>" /// SIMPLEST WAY 
  
-//render lead type1
-function renderlead()
-{
- let listItem=""
-    // log out the item in the my leasds array using for loop
- for (let i=0; i < myLeads.length;i++)
-{
-// listItem += "<li><a href='" + myLeads[i] + "' target='_blank'>" + myLeads[i] + "</a></li>";
- listItem += `<li><a href="${myLeads[i]}"target='blank'>
- ${myLeads[i]}</a></li>`;
-   
-}
-ulEl.innerHTML= listItem
 
-}
+
+ 
+//local storage 
+//  localStorage.setItem("myLeads","samplelead.com")
+//  localStorage.getItem("myleads")
+//  console.log("myleads")
+// localStorage.clear()
+
+//   push strings to the array ... using json stringify and JSON.PARSE
+//   let myLead = ["www.loval.ciom"]
+
+// // convert array → string
+// let jsonString = JSON.stringify(myLead)
+
+// // now parse it back → array
+// myLead = JSON.parse(jsonString)
+
+// myLead.push("www.leaad2.com")
+
+// console.log(myLead)
